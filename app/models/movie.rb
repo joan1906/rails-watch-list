5825dc268e64cea48a045311b77177897b539053
joan blanco <joanblanco1906@gmail.com>
2021-10-21 15:52:45 -0400

    CRUD listo

---
+++ app/models/movie.rb
@@ -0,0 +1,7 @@
+class Movie < ApplicationRecord
+    has_many :bookmarks
+
+    validates :title, uniqueness: true
+    validates :title, presence: true
+    validates :overview, presence: true
+end
